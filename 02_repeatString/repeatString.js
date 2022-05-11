26631b4f90b8c0479f9e4d213c6aee1c4c3e43cd
Kaleb Moore <k4l3bmoore@gmail.com>
2022-05-10 19:13:34 -0600

    Create repeatstring function that passes all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let answer = string;
+    if (num === 0) {
+        return ""
+    } else if (num < 0) {
+        return "ERROR"
+    } else {
+        for (let i = 1; i < num; i++) {
+            answer += string;
+        }
+    }
+    return answer;
 };
 
 // Do not edit below this line
