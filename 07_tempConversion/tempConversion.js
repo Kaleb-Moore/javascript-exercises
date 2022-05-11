5911fd355a90ad290d18bc2e9f0ff0e5382e8fc9
Kaleb Moore <k4l3bmoore@gmail.com>
2022-05-11 17:29:28 -0600

    Create Celius fahrenheit conversions that pass tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let newTemp = ((temp - 32) * (5/9));
+  let round = Math.round(newTemp * 10) / 10;
+  return round;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let newTemp = ((temp * (9/5) + 32));
+  let round = Math.round(newTemp * 10) / 10;
+  return round;
 };
 
 // Do not edit below this line
