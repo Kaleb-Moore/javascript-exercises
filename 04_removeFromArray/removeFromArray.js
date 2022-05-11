343b3e0e647fb81206b5a331b2281ad5f9a2c877
Kaleb Moore <k4l3bmoore@gmail.com>
2022-05-10 20:05:36 -0600

    Create removeFromArray and pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    for (let arg of args) {
+        for (let i = 0; i != -1; i) {
+            if (array.indexOf(arg) === -1) {
+                i = -1;
+            } else {
+                array.splice(array.indexOf(arg), 1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
