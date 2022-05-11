b884d3313a6d4eeab9b12ea50b319c68d0480504
Kaleb Moore <k4l3bmoore@gmail.com>
2022-05-11 16:53:45 -0600

    Create leapyear function that passes tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    let mod4 = year % 4;
+    let mod100 = year % 100;
+    let mod400 = year % 400;
+    
+    if ((mod100 === 0 && mod400 === 0)) {
+        return true;
+    } else if ((mod4 === 0 && mod100 != 0)) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
