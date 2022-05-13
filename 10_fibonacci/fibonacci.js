9503e3f136277b2aea2ec35fc9852b45f1e4e101
Kaleb Moore <k4l3bmoore@gmail.com>
2022-05-13 17:53:55 -0600

    Create Fibonacci sequence calculator that passes tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let array = [1, 1];
+    let fibonacciNum;
+    if (parseInt(num) === 1 || parseInt(num) === 2) {
+        return 1;
+    }else if (parseInt(num) <= 0) {
+        return "OOPS"
+    } else {
+        for (let i = 2; i < parseInt(num); i++) {
+            let newNum = (array[i-2] + array[i-1]);
+            array.push(newNum);
+            fibonacciNum = array[parseInt(num)-1];
+        }
+        return fibonacciNum;
+    }
 };
 
 // Do not edit below this line
