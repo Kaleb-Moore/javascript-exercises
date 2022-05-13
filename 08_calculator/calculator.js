b39b9a0ddc41ec7005bb93ce5a458b6234606fe6
Kaleb Moore <k4l3bmoore@gmail.com>
2022-05-13 13:05:49 -0600

    Create Function for Add, Subtract, sum that all pass tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,17 +1,31 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  let total = num1 + num2;
+  return total;	
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  let total = num1 - num2;
+  return total;	
 };
 
-const sum = function() {
-	
+const sum = function([...arg]) {
+	let total = 0;
+  if (arg == undefined) {
+    return total;
+  } else {
+    for (let i = 0; i < arg.length; i++) {
+      total += arg[i];
+    }
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(num1, ...arg) {
+  let total = num1;
+  for (let i = 0; i < arg.length; i ++) {
+    total *= arg[i];
+  }
+  return total;
 };
 
 const power = function() {
