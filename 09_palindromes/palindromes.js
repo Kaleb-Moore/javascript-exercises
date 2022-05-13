1d8f10f7c6fa811d201c50df42f9b7598d5f3a83
Kaleb Moore <k4l3bmoore@gmail.com>
2022-05-13 17:26:26 -0600

    Create Function that assess if a string is a palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let spiltString = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+    .toLowerCase()
+    .split('')
+    .reverse()
+    .join('')
+    .split(' ')
+    .join('');
 
+    let newString = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase().split(' ').join('');
+    console.log(spiltString, newString)
+    if (spiltString === newString){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
